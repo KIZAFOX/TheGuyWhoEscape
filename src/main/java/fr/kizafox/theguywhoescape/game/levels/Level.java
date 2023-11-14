67be0c2b339799a779faaeb23a9b5c3f3b8819ac
KIZAFOX <victor.pernak@hotmail.fr>
2023-11-14 09:41:03 +0100

    :pencil2: file updated to record

--- src/main/java/fr/kizafox/theguywhoescape/game/levels/Level.java
+++ src/main/java/fr/kizafox/theguywhoescape/game/levels/Level.java
@@ -7,19 +7,9 @@ package fr.kizafox.theguywhoescape.game.levels;
  * @date : 04/07/2023
  * @project : TheGuyWhoEscape
  */
-public class Level {
-
-    private final int[][] levelData;
-
-    public Level(int[][] levelData) {
-        this.levelData = levelData;
-    }
+public record Level(int[][] levelData) {
 
     public int getSpriteIndex(final int x, final int y) {
         return this.levelData[y][x];
-    }
-
-    public int[][] getLevelData(){
-        return levelData;
     }
 }
